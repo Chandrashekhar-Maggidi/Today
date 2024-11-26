bf078ba5930744309104ac9af8e76fd7321fab02
Chandrashekhar <chandrashekharmaggidi6@gmail.com>
2024-11-26 05:43:07 +0530

    Edit and commit

---
+++ src/test/java/com/Hrms/tests/TestClass2.java
@@ -0,0 +1,11 @@
+package com.Hrms.tests;
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I am a Tester");
+	}
+
+}
